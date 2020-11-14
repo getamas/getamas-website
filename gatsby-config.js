b1e14b10cbe8ed0c019775f1e756a5fa34b80c21
Gerényi Tamás <g.tamas@y-collective.hu>
2020-11-14 18:29:57 +0100

    init repo

---
+++ gatsby-config.js
@@ -0,0 +1,117 @@
+module.exports = {
+  siteMetadata: {
+    // Site URL for when it goes live
+    siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
+    // Your Name
+    name: 'Tamás Gerényi',
+    // Main Site Title
+    title: `Frontend Developer | Tamás Gerényi`,
+    // Headline
+    headline: `Frontend Developer`,
+    // Description that goes under your name in main bio
+    description: `A passionate, highly motivated web developer, having extensive knowledge in developing large-scale applications for the modern web.`,
+    // Optional: Twitter account handle
+    author: ``,
+    // Optional: Github account URL
+    github: `https://github.com/getamas`,
+    // Optional: LinkedIn account URL
+    linkedin: `https://www.linkedin.com/in/tamasgerenyi`,
+    // Content of the About Me section
+    about: `My key skills include but not limited to sitebuilding responsive website layouts, creating complex JavaScript solutions, single-page applications and architecturing web-based systems.`,
+    // Optional: List your projects, they must have `name` and `description`. `link` is optional.
+    projects: [
+      {
+        name: 'Devfolio',
+        description: 'A zero-config and blazing fast personal site + blog built with GatsbyJs and TailwindCSS',
+        link: 'https://github.com/RyanFitzgerald/devfolio',
+      },
+    ],
+    // Optional: List your experience, they must have `name` and `description`. `link` is optional.
+    experience: [
+      {
+        name: 'Y-collective',
+        description: 'Frontend Developer, Sep 2018 - Present',
+        link: ''
+      },
+      {
+        name: 'Webstar',
+        description: 'Software Tester, Mar 2017 - Apr 2018',
+        link: ''
+      }
+    ],
+    // Optional: List your skills, they must have `name` and `description`.
+    skills: [
+      {
+        name: 'Languages & Frameworks',
+        description:
+          'JavaScript, Vue.js, AngularJS, HTML5/SCSS, Git',
+      },
+      {
+        name: 'Other',
+        description: 'Redmine, Planning, JIRA, Trello, Pair Programming, Agile / Scrum',
+      },
+    ],
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+              wrapperStyle: `margin: 0 0 30px;`,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `ADD YOUR TRACKING ID HERE`, // Optional Google Analytics
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `devfolio`,
+        short_name: `devfolio`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`, // This color appears on mobile
+        display: `minimal-ui`,
+        icon: `src/images/icon.png`,
+      },
+    },
+  ],
+};
